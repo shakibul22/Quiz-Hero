0474c12c60f4dcf5911e63059190052985ecbc48
Shakibul Islam <shakibulislam684@gmail.com>
2023-03-16 22:05:27 +0600

    Blog questions answer

--- blog.js
+++ blog.js
@@ -1,12 +1,12 @@
 
-document.getElementById('dropdownRadioButton').addEventListener('click', () => {
-    document.getElementById('submitContainer').style.display = 'none';
+// document.getElementById('blog-button').addEventListener('click', () => {
+//     const container=document.getElementById('rulesContainer').style.display = 'none';
 
-    document.getElementById('blogs').style.display = 'block';
+//     document.getElementById('blogs').style.display = 'block';
+// })
+// document.getElementById('blog-button').addEventListener('click', () => {
+//     document.getElementById('rulesContainer').style.display = 'flex';
+//     document.getElementById('blogs').style.display = 'none';
 
-})
-document.getElementById('dropdownRadioButton').addEventListener('click', () => {
-    document.getElementById('submitContainer').style.display = 'flex';
-    document.getElementById('blogs').style.display = 'none';
+// })
 
-})
